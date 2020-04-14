8bc5e1e1e147904639f638643c85e5e2020b8d14
sabyasachi.nandy <sabya.nandy14@gmail.com>
2020-04-15 00:14:15 +0530

    ADD | Added docker compose for solr, and created basic solr project template

---
+++ data-consumer/src/test/java/com/neel/services/SolrWriterTest.java
@@ -0,0 +1,14 @@
+package com.neel.services;
+
+import org.apache.solr.client.solrj.embedded.EmbeddedSolrServer;
+import org.junit.jupiter.api.Test;
+
+
+class SolrWriterTest{
+
+    @Test
+    void shouldWriteBusDataIntoSolr() {
+//        EmbeddedSolrServer solrServer = new EmbeddedSolrServer();
+
+    }
+}
